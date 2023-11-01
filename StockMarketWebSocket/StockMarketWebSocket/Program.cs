15437242f4655945886ff2a695888b537bbb8fc7
SSpedsbjerg <sispe20@student.sdu.dk>
2023-11-01 14:49:02 +0100

    started on websocket and moved worker.yml

---
+++ StockMarketWebSocket/StockMarketWebSocket/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Server {
+    public static void Main(string[] args) {
+        string ip = "127.0.0.1";
+        int port = 80;
+        var server = new TcpListener(IPAddress.Parse(ip), port);
+
+        server.Start();
+        Console.WriteLine($"Server has started, waiting for connection, IP: {ip}, Port: {port}");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine($"Client connected, {client.Connected}");
+    }
+}

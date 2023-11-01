18da9b9c23f96cd890f13b0e69d03aaf0e4c7403
SSpedsbjerg <sispe20@student.sdu.dk>
2023-11-01 13:26:38 +0100

    FirstEdition
    only includes a simple API, and not a finished one yet

---
+++ TwitterBitcoinAPI/TwitterBitcoinAPI/Controllers/Tweet.cs
@@ -0,0 +1,61 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace TwitterBitcoinAPI.Controllers {
+    public class Tweet {
+        [Name("Date")]
+        public DateTime Date {
+            get {
+                return DateTime;
+            }
+            set {
+                this.DateTime = value;
+            }
+        }
+
+        [Name("text")]
+        public string TweetText {
+            get {
+                return Text;
+            }
+            set {
+                Text = value;
+            }
+        }
+
+        [Name("Sentiment")]
+        public string Sentiment {
+            get {
+                if (Positive is true) {
+                    return "Positive";
+                }
+                else if (Positive is not true){
+                    return "Negative";
+                }
+                else {
+                    return "Other";
+                }
+                
+            }
+            set {
+                if(value.ToLower().Equals("Positive") || value.ToLower().Equals("True")) {
+                    Positive = true;
+                }
+                else if (value.ToLower().Equals("Negative") ||value.ToLower().Equals("False")) {
+                    Positive = false;
+                }
+                else {
+                    Positive = false;
+                }
+            }
+        }
+
+
+        private DateTime DateTime;
+        private string Text;
+        private bool Positive;
+
+        public override string ToString() {
+            return $"Date: {DateTime.ToString()}, Text: {Text.ToString()}, Sentiment: {Positive.ToString()}";
+        }
+    }
+}
---
+++ TwitterBitcoinAPI/TwitterBitcoinAPI/Controllers/TweetController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterBitcoinAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TweetController : ControllerBase {
+        [HttpGet]
+        [Route("/")]//main page, used to check connection
+        public Task<string> ConnectionCheck() {
+            return Task.FromResult("200");
+        }
+
+        [HttpGet]
+        [Route("/GetLatestTweets/{number}")]
+        public async Task<string> GetLatestTweets(string number) {
+            int _number;
+            try {
+                _number = int.Parse(number);
+            }
+            catch {
+                return "400";
+            }
+            CsvDataExtractor csvDataExtractor = new CsvDataExtractor();
+            List<Tweet> tweets = (List<Tweet>)csvDataExtractor.ExtractDataFromCsv("C:/Users/simon/source/repos/TwitterBitcoinAPI/mbsa.csv", _number);
+            return tweets[0].ToString();
+        }
+    }
+}

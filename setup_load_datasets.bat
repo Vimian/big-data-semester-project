ecf2906afc16079f3e5e6af6bd2249cd5770687f
SaltIncluded <43348526+casperfj@users.noreply.github.com>
2023-12-05 03:48:05 +0100

    Finished the dataset loader

--- setup_load_datasets.bat
+++ setup_load_datasets.bat
@@ -1,17 +1,14 @@
-# create dataset loader
+ECHO off
+cls
 
-```
+ECHO Setup dataset loader
+ECHO.
+
 docker build -t dataset-loader -f ./mockup/dataset-loader/Dockerfile .
-```
 
-```
 kubectl create namespace mockup
 kubectl apply -f ./mockup/dataset-loader/dataset-loader.yaml -n mockup
-```
 
-# clean up
-
-```
-kubectl delete -f ./mockup/dataset-loader/dataset-loader.yaml -n mockup
-kubectl delete namespace mockup
-```
+ECHO.
+ECHO Setup dataset loader successful
+pause

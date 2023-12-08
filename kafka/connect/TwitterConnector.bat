11f0d24f96f40136462f36e0757d302c483e0438
Simon Spedsbjerg <simon@spedsbjerg.dk>
2023-12-08 18:40:56 +0100

    this

--- kafka/connect/TwitterConnector.bat
+++ kafka/connect/TwitterConnector.bat
@@ -5,6 +5,11 @@ ECHO Build docker image
 ECHO.
 docker image build --tag twitter-connector .
 
+ECHO Run docker image
+ECHO.
+docker run twitter-connector
+
+
 ECHO Create Namespace Kafka
 ECHO.
 kubectl create namespace kafka
@@ -22,10 +27,6 @@ kubectl apply -f KafkaConnectConfig.yaml -n kafka
 ECHO Apply Twitter Connection
 ECHO.
 kubectl apply -f TwitterCon.yaml -n kafka
-
-ECHO Run docker image
-ECHO.
-docker run twitter-connector
 
 ECHO.
 ECHO Setup successful
